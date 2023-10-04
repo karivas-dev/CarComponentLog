1be3bfd7b5369aecc548847caed5bcc2d515e479
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-10-04 11:12:42 -0600

    Added parameters to Display and NoDisplay

--- src/Display.js
+++ src/Display.js
@@ -1,18 +1,26 @@
-import {Pressable, ScrollView, Text} from "react-native";
+import {FlatList, Pressable, ScrollView, Text, View} from "react-native";
 import CardPiece from "./CardPiece";
+import {useState} from "react";
+import DetailedCardPiece from "./DetailedCardPiece";
 
-const Display = ({modalVisibleForm, setModalVisibleForm}) => {
+const Display = ({modalVisibleForm, setModalVisibleForm, parts, erasePart}) => {
+    const [currentPart, setCurrentPart] = useState(null);
+    const [modalVisibleDetails, setModalVisibleDetails] = useState(false);
+
     return (
-        <ScrollView className="flex-1 m-5">
+        <View className="flex-1 m-5">
             <Text className="text-white text-center text-2xl my-5 font-extrabold">Car Component Log</Text>
 
             <Pressable className="py-3 px-12 my-4 rounded-full bg-[#ee9442]"
                        onPress={() => {setModalVisibleForm(!modalVisibleForm)}}>
                 <Text className="text-white font-bold text-center">Agregar pieza</Text>
             </Pressable>
-
-            <CardPiece />
-        </ScrollView>
+            <FlatList data={parts}
+                      renderItem={({item}) => <CardPiece part={item} erasePart={erasePart} setCurrentPart={setCurrentPart} setModalVisibleDetails={setModalVisibleDetails}/>}
+                      keyExtractor={part => part.id} nestedScrollEnabled/>
+            <DetailedCardPiece modalVisibleDetails={modalVisibleDetails} setModalVisibleDetails={setModalVisibleDetails}
+                part={currentPart} setCurrentPart={setCurrentPart} erasePart={erasePart}/>
+        </View>
     );
 }
 
--- src/NoDisplay.js
+++ src/NoDisplay.js
@@ -1,6 +1,6 @@
 import {Image, Pressable, Text, View} from "react-native";
 
-const Display = () => {
+const Display = ({modalVisibleForm, setModalVisibleForm}) => {
     return (
         <View className="flex-1 items-center justify-center m-5">
             <View className="bg-amber-500 rounded-full">
@@ -10,7 +10,8 @@ const Display = () => {
             </View>
             <Text className="text-white text-2xl my-5 font-extrabold">Car Component Log</Text>
             <Text className="text-white mb-4">No se han registrado piezas</Text>
-            <Pressable className="py-3 px-12 rounded-full bg-[#ee9442]">
+            <Pressable className="py-3 px-12 rounded-full bg-[#ee9442]"
+                       onPress={() => setModalVisibleForm(!modalVisibleForm)}>
                 <Text className="text-white font-bold">Agregar pieza</Text>
             </Pressable>
         </View>
